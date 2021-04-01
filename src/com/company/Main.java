94670ab203b4a915fea7ae445d289e95d10375d8
pandzia97 <andzia.okarmus@gmial.com>
2021-04-01 17:32:33 +0200

    Added code for project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("BMI Calculator");
+        System.out.println("Enter your growth: ");
+        double growth = scanner.nextDouble();
+        System.out.println("Enter your weight: ");
+        double weight = scanner.nextDouble();
+        double bmi = weight / (growth * growth);
+
+        if (bmi < 18.5) {
+            System.out.println("You have underweight");
+        } else if (18.5 < bmi && bmi < 24.8) {
+            System.out.println("Good");
+        } else if (25 < bmi && bmi < 29.9) {
+            System.out.println("You have overweight");
+        } else {
+            System.out.println("You are fat");
+        }
     }
 }
