659b24e602bbb91fa2e05d9d8976f7f2e3c780f2
pandzia97 <andzia.okarmus@gmial.com>
2021-04-01 17:51:43 +0200

    Enlargemented option in if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,14 +13,26 @@ public class Main {
         double weight = scanner.nextDouble();
         double bmi = weight / (growth * growth);
 
-        if (bmi < 18.5) {
-            System.out.println("You have underweight");
-        } else if (18.5 < bmi && bmi < 24.8) {
+        if (bmi < 8.5) {
+            System.out.println("Underweight");
+        } else if (bmi < 16) {
+            System.out.println("Starvation");
+        } else if (bmi < 17) {
+            System.out.println("Emaciation");
+        } else if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (18.5 < bmi && bmi > 24.9) {
             System.out.println("Good");
-        } else if (25 < bmi && bmi < 29.9) {
-            System.out.println("You have overweight");
-        } else {
-            System.out.println("You are fat");
+        } else if (25 < bmi && bmi > 24.9) {
+            System.out.println("Overweight");
+        } else if (bmi >= 30) {
+            System.out.println("Obesity");
+        } else if (bmi >= 34.9) {
+            System.out.println("1 level obesity");
+        } else if (bmi >= 39.9) {
+            System.out.println("2 level obesity");
+        } else if (bmi > 40) {
+            System.out.println("3 level obesity");
         }
     }
 }
