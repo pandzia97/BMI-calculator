d5bfd2eae0683f72ef9bbcd6005b5982ad392c61
pandzia97 <andzia.okarmus@gmial.com>
2021-04-02 18:01:07 +0200

    Corrected if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("BMI Calculator");
@@ -13,26 +12,27 @@ public class Main {
         double weight = scanner.nextDouble();
         double bmi = weight / (growth * growth);
 
-        if (bmi < 8.5) {
+        if (bmi < 18.5) {
             System.out.println("Underweight");
-        } else if (bmi < 16) {
-            System.out.println("Starvation");
         } else if (bmi < 17) {
             System.out.println("Emaciation");
-        } else if (bmi < 18.5) {
+        } else if (bmi < 16) {
+            System.out.println("Starvation");
+        } else if (bmi < 8.5) {
             System.out.println("Underweight");
-        } else if (18.5 < bmi && bmi > 24.9) {
+        } else if (18.5 < bmi && bmi < 24.9) {
             System.out.println("Good");
-        } else if (25 < bmi && bmi > 24.9) {
+        } else if (25 < bmi && bmi < 24.9) {
             System.out.println("Overweight");
-        } else if (bmi >= 30) {
-            System.out.println("Obesity");
-        } else if (bmi >= 34.9) {
-            System.out.println("1 level obesity");
-        } else if (bmi >= 39.9) {
-            System.out.println("2 level obesity");
         } else if (bmi > 40) {
             System.out.println("3 level obesity");
+        } else if (bmi <= 34.9) {
+            System.out.println("1 level obesity");
+        } else if (bmi <= 39.9) {
+            System.out.println("2 level obesity");
+        } else if (bmi <= 30) {
+            System.out.println("Obesity");
         }
+
     }
 }
