c7acb025b4e850e89802c870e5a21d3472160732
pandzia97 <andzia.okarmus@gmial.com>
2021-04-02 21:28:38 +0200

    corrected if v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,23 +12,25 @@ public class Main {
         double weight = scanner.nextDouble();
 
         double bmi = weight / (growth * growth);
-
+        System.out.println(bmi);
         if (bmi >= 40) {
             System.out.println("3 level obesity");
-        } else if (bmi < 40) {
+        } else if (bmi > 36) {
             System.out.println("2 level obesity");
-        } else if (bmi < 36) {
+        } else if (bmi > 24.9) {
             System.out.println("1 level obesity");
-        } else if (bmi < 24.9) {
+        } else if (bmi > 18.5) {
             System.out.println("Overweight");
-        } else if (bmi < 18.5) {
+        } else if (bmi > 17) {
             System.out.println("Underweight");
-        } else if (bmi < 17) {
+        } else if (bmi > 16) {
             System.out.println("Emaciation");
-        } else if (bmi < 16) {
+        } else if (bmi > 8.5) {
             System.out.println("Starvation");
-        } else if (bmi < 8.5) {
+        } else if (bmi > 0) {
             System.out.println("Underweight");
+        } else {
+            System.out.println("error");
         }
     }
 }
